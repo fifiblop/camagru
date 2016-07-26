6944d0c83ffb09828d84a0ccddf6429de19b0fe1
Philippe DELEFOSSE <pdelefos@student.42.fr>
2016-07-26 17:47:40 +0200

    starting front

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Camagru</title>
+	<link rel="icon" type="image/gif" href="ressources/favicon.ico" sizes="16x16"/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+	<nav>
+		<a id="logo" href="#">Camagru</a>
+	</nav>
+</header>
--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php
+	include("header.php");
+?>
 
 </body>
 </html>
