cd142236eecffb2678b66a8c6830cc2401158015
Philippe DELEFOSSE <pdelefos@student.42.fr>
2016-08-09 19:46:28 +0200

    User not a class anymore

---
+++ classes/User.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../config/database.php';
+
+function create_user($login, $email, $password) {
+	$bdd = database_connect();
+	$sql = "INSERT INTO user (login, email, password)
+	VALUES (:login, :email, :password);";
+	$stmt = $bdd->prepare($sql);
+	$stmt->bindParam(':login', $login, PDO::PARAM_STR);
+	$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+	$stmt->bindParam(':password', $password, PDO::PARAM_STR);
+	$stmt->execute();
+}
+
+function user_exists($login) {
+	$bdd = database_connect();
+	$sql = "SELECT id FROM user WHERE login = :login;";
+	$stmt = $bdd->prepare($sql);
+	$stmt->bindParam(':login', $login, PDO::PARAM_STR);
+	$stmt->execute();
+	$count = $stmt->rowCount();
+	if ($count == 1)
+		return (true);
+	return (false);
+}
+?>
